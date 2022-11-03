8cf36a3ab5bde54c0f42509df60def7b40bdf136
Katkauske <kkatkauske@gmail.com>
2022-11-03 11:35:15 +0200

    Pirma uzduotis

---
+++ Programa1.java
@@ -0,0 +1,21 @@
+public class Programa1 {
+    public static void main(String[] args) {
+
+        System.out.println(sk1);
+        System.out.println(sk2);
+        System.out.println(sk3);
+        System.out.println(sk4);
+        System.out.println(sk5);
+        System.out.println(sk6);
+        System.out.println(sk7);
+
+
+        int sk1 = 39;
+        short sk2 = 5896;
+        double sk3 = 6.13235;
+        long sk4 = (long)sk1;
+        int sk5 = (int)sk2;
+        byte sk6 = (byte)sk2;
+        int sk7 = (int)sk3;
+    }
+}
---
+++ src/Programa1.java
@@ -0,0 +1,29 @@
+public class Programa1 {
+    public static void main(String[] args) {
+
+        int sk1 = 39;
+        System.out.println(sk1);
+        short sk2 = 5896;
+        System.out.println(sk2);
+        double sk3 = 6.13235;
+        System.out.println(sk3);
+
+        long sk4 = (long)sk1;
+        System.out.println(sk4);
+        int sk5 = (int)sk2;
+        System.out.println(sk5);
+        byte sk6 = (byte)sk2;
+        System.out.println(sk6);
+        int sk7 = (int)sk3;
+        System.out.println(sk7);
+
+
+        char pirmas = 'a';
+        char antras = 'b';
+        char trecias = 'c';
+        System.out.println("pirmas + antras + trecias = " + pirmas + antras + trecias);
+
+
+    }
+
+}
